146f7e6d3d3eb72dd6d121e6ef9216fe14fd6b0d
ytworks <test@test.com>
2017-07-21 14:35:42 +0900

    Release 0.1

---
+++ DICOMReader.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+# coding:utf-8
+from __future__ import print_function
+import dicom
+import numpy as np
+import sys
+import os
+
+
+def dicom_to_np(mri_file_path):
+    # Extracting data from the mri file
+    plan = dicom.read_file(mri_file_path)
+    image_2d = np.array(plan.pixel_array)
+    phototype = plan.PhotometricInterpretation
+    Bits = plan.WindowWidth
+    if phototype == 'MONOCHROME1':
+        image_2d = float(Bits - 1) - image_2d
+    return np.array(image_2d), Bits
+
+
+
+
+if __name__ == '__main__':
+    DICOM_PATH = './000136.dcm'
+
+    X, _ = dicom_to_np(mri_file_path = DICOM_PATH)
+    print(X)
